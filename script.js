5533392e73f85c103be455a69da839bf0d43ef53
proRamLOGO <kapilgupta547@outlook.com>
2020-08-23 01:33:12 +0530

    Final

--- script.js
+++ script.js
@@ -6,8 +6,6 @@ const input = document.getElementById('video') ;
 
 Promise.all([
     faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
-    faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-    faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
     faceapi.nets.faceExpressionNet.loadFromUri('/models')
 ]).then(startVideo);
 
